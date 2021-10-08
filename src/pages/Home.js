e55a722b04d8dcf654ccb9cbdc6b8eabd9a50038
DenquinMaxence <denquin.maxence.pro@gmail.com>
2021-10-08 11:39:05 +0200

    left-pard done, right-part wip

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Profil from '../media/profil.png';
 
 const Home = () => {
@@ -5,123 +6,80 @@ const Home = () => {
 		<div className='main'>
 			<div className='left-part'>
 				<div className='profil-header'></div>
+				<div className='home-background'></div>
 				<div className='profil-pic'>
 					<img src={Profil} alt='profil-pic' />
 				</div>
 				<div className='profil-content'>
 					<h2 className='profil-name'>Maxence Denquin</h2>
-					<span className='profil-description'>Développeur full-stack</span>
+					<span className='profil-description'>Développeur web full-stack</span>
 					<div className='profil-about'>
-						<box-icon name='calendar' flip='horizontal' color='#3991a5' />
-						<span>09 Septembre 2000</span>
-						<box-icon name='phone' type='solid' flip='horizontal' color='#3991a5' />
-						<span>
-							<a href='tel:+06.77.59.83.10'>06.77.59.83.10</a>
-						</span>
-
-						<box-icon name='home' type='solid' flip='horizontal' color='#3991a5' />
-						<span>Outreau, 62230</span>
-						<box-icon name='envelope' color='#3991a5' />
-						<span>
-							<a href='mailto:denquin.maxence.pro@gmail.com'>
-								denquin.maxence.pro@gmail.com
-							</a>
-						</span>
+						<div>
+							<box-icon name='calendar' flip='horizontal' color='#3991a5' />
+							<span>09/09/2000</span>
+						</div>
+						<div>
+							<box-icon name='phone' type='solid' flip='horizontal' color='#3991a5' />
+							<span>
+								<a href='tel:+06.77.59.83.10'>06.77.59.83.10</a>
+							</span>
+						</div>
+						<div>
+							<box-icon name='home' type='solid' flip='horizontal' color='#3991a5' />
+							<span>Outreau, 62230</span>
+						</div>
+						<div>
+							<box-icon name='envelope' color='#3991a5' />
+							<span>
+								<a href='mailto:denquin.maxence.pro@gmail.com'>Me contactez</a>
+							</span>
+						</div>
 					</div>
 				</div>
 				<div className='content'>
-					<hr />
-					<div className="card">
-						<div className="subcard">Experience</div>
-						<div className="subcard">Background</div>
-						<div className="subcard">Interest</div>
-						<div className="subcard">Portfolio</div>
-					</div>
-					<div className='informations'>
-						<h1>Informations</h1>
-						<p>
-							<span className='label'>Nom & Prénom</span>
-							<br />
-							<span>Denquin Maxence</span>
-						</p>
-						<p>
-							<span className='label'>Date de naissance</span>
-							<br />
-							<span>09 Septembre 2000, 21 ans</span>
-						</p>
-						<p>
-							<span className='label'>Secteur</span>
-							<br />
-							<span>Boulogne-sur-Mer</span>
-						</p>
-						<p>
-							<span className='label'>Situation</span>
-							<br />
-							<span>Célibataire</span>
-						</p>
-						<p>
-							<span className='label'>Langues</span>
-							<br />
-							<span>Français - Langue maternelle</span>
-							<br />
-							<span>Anglais - Écrit et conversation</span>
-						</p>
-						<p>
-							<span className='label'>Mobilités</span>
-							<br />
-							<span>Inscrit pour passer le code la route</span>
-							<br />
-							<span>Transport en commun, voiture</span>
-						</p>
-					</div>
-					<div className='hobbies'>
-						<hr />
-						<h1>Centres d'intérêt</h1>
-						<ul>
-							<li>Jeux en réseau</li>
-							<li>Développement informatique</li>
-						</ul>
-					</div>
-					<div className='social'>
-						<hr />
-						<h1>Réseaux sociaux</h1>
-						<ul>
-							<li>
-								Facebook :{' '}
-								<a href='https://www.facebook.com/maxence.denquin.7/'>
-									maxence.denquin7
-								</a>
-							</li>
-							<li>
-								LinkedIn :{' '}
-								<a href='https://www.linkedin.com/in/maxence-denquin-2a12431b3/'>
-									Maxence Denquin
-								</a>
-							</li>
-							<li>
-								Discord : <a href='https://discord.com/'>Kris Kaitch#7375</a>
-							</li>
-						</ul>
-					</div>
-					<div className='contact'>
-						<hr />
-						<h1>Contact</h1>
-						<p>
-							<span className='label'>Téléphone</span>
-							<br />
-							<span>06 77 59 83 10</span>
-						</p>
-						<p>
-							<span className='label'>Email</span>
-							<br />
-							<span>denquin.maxence.pro@gmail.com</span>
-						</p>
+					<div className='card'>
+						<Link to='#about' className='subcard'>
+							<box-icon name='package' size='lg' color='white'></box-icon>
+							<span>À propos</span>
+						</Link>
+						<Link to='#experiences' className='subcard'>
+							<box-icon name='briefcase' size='lg' color='white'></box-icon>
+							<span>Expériences</span>
+						</Link>
+						<Link to='#formation' className='subcard'>
+							<box-icon name='code-block' size='lg' color='white'></box-icon>
+							<span>Formations</span>
+						</Link>
+						<Link to='#skills' className='subcard'>
+							<box-icon name='folder-plus' size='lg' color='white'></box-icon>
+							<span>Compétences</span>
+						</Link>
 					</div>
 				</div>
 			</div>
 			<div className='right-part'>
 				<div className='header'>
-					<h1>Développeur web junior</h1>
+					<h3 className='job-title'>Développeur web full-stack</h3>
+					<div id='about' className='about'>
+						<h3 className='about-title'>À propos</h3>
+						<p className='about-text'>
+							Passionné par le développement informatique des des années, mon ambition
+							serait que ma passion devienne mon métier. Évoluer au fil des
+							technologies en concrétisant les acquis tout en s'ouvrant sur les
+							nouveaux outils et pour moi la chose la plus passionnante et
+							intéressante à faire dans ce domaine.
+						</p>
+					</div>
+					<hr />
+					<div className='experiences'>
+						<h4 className='experience-title'>Expériences Professionnelles</h4>
+						<div className="card">
+							<div className="card-body">
+								<h3 className="card-title">Développeur full-stack</h3>
+								<h4 className="card-subtitle">Simplon.co</h4>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
