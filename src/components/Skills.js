c655bd8e6d66e22f933499f33cf0d48cc93f14d2
DenquinMaxence <denquin.maxence.pro@gmail.com>
2021-10-08 16:39:50 +0200

    mobile version done

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,3 +1,5 @@
+import { SkillsData } from '../data/Skills';
+
 const Skills = () => {
 	return (
 		<>
@@ -5,15 +7,38 @@ const Skills = () => {
 			<div className='skills'>
 				<h4 className='skill-title'>Compétences</h4>
 				<div className='card'>
-					<div className='card-body'>
-						<div className='card-img'>
-							<box-icon name='html5' type='logo' size='lg' color='#dd4b25' />
-							HTML5
-						</div>
-						<div className='progressbar-block progressbar-moved'>
-							<div className='progressbar-set' style={{ width: '35%' }}></div>
-						</div>
-					</div>
+					<h4 className='skill-subtitle'>Langages et frameworks</h4>
+					{SkillsData.languages_frameworks.length > 0 &&
+						SkillsData.languages_frameworks.map((lang_fram, index) => {
+							return (
+								<div className='card-body' key={index}>
+									<div className='card-img'>{lang_fram.icon}</div>
+									<div className='progressbar'>
+										<h5 className='card-title'>{lang_fram.title}</h5>
+										<div className='progressbar-block progressbar-moved'>
+											<div className='progressbar-set'></div>
+										</div>
+									</div>
+								</div>
+							);
+						})}
+				</div>
+				<div className='card'>
+					<h4 className='skill-subtitle'>Outils</h4>
+					{SkillsData.tools.length > 0 &&
+						SkillsData.tools.map((tool, index) => {
+							return (
+								<div className='card-body' key={index}>
+									<div className='card-img'>{tool.icon}</div>
+									<div className='progressbar'>
+										<h5 className='card-title'>{tool.title}</h5>
+										<div className='progressbar-block progressbar-moved'>
+											<div className='progressbar-set'></div>
+										</div>
+									</div>
+								</div>
+							);
+						})}
 				</div>
 			</div>
 		</>
